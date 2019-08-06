69c3286bfdc573a14ea50afff0652be668bee356
Archer <archerhan1990@outlook.com>
2019-08-06 18:55:49 +0800

    push详情页面开发

--- lib/page/repo/push_detail_page.dart
+++ lib/page/repo/push_detail_page.dart
@@ -4,7 +4,11 @@
  *  description :
  */
 
+import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:codehub/widget/repo/sliver_header_delegate.dart';
+import 'package:codehub/widget/repo/card_item.dart';
 
 class PushDetailPage extends StatefulWidget {
   final String userName;
@@ -21,9 +25,74 @@ class PushDetailPage extends StatefulWidget {
   _PushDetailPageState createState() => _PushDetailPageState();
 }
 
-class _PushDetailPageState extends State<PushDetailPage> {
+class _PushDetailPageState extends State<PushDetailPage>
+    with TickerProviderStateMixin {
+  _renderItem(e) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Text("path"),
+        ),
+        Expanded(
+          child: CardItem(
+            margin: EdgeInsets.all(10),
+            child: Container(
+              height: 30,
+              width: 300,
+              child: Text("file"),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.reposName),
+      ),
+      body: EasyRefresh(
+        onRefresh: null,
+        onLoad: null,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverPersistentHeader(
+              delegate: SliverHeaderDelegate(
+                snapConfig: FloatingHeaderSnapConfiguration(
+                  vsync: this,
+                  curve: Curves.bounceInOut,
+                  duration: const Duration(milliseconds: 10),
+                ),
+                minHeight: 120,
+                maxHeight: 120,
+                child: CardItem(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.black87,
+                  child: Container(
+                    height: 120,
+                    child: Text(
+                      "this is the header",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // SliverList(
+            //   delegate: SliverChildBuilderDelegate(
+            //     (BuildContext context, int index) {
+            //       return _renderItem(null);
+            //     },
+            //     childCount: 10,
+            //   ),
+            // ),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
